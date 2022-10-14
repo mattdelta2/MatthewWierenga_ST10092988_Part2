8af5b11ac972f77449e241b5f8aaaf3797026a78
Matthew Wierenga <matt.wierenga@outlook.com>
2022-10-14 13:00:56 +0200

    Create Mage.cs

---
+++ MatthewWierenga_ST10092988_Part2/Mage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatthewWierenga_ST10092988_Part2
+{
+    internal class Mage
+    {
+    }
+}
