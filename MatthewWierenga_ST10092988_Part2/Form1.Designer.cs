a3180c763b06adea3080e52cd39c66bfbba17830
Matthew Wierenga <matt.wierenga@outlook.com>
2022-10-14 12:54:43 +0200

    made sure everything was running like part 1

--- MatthewWierenga_ST10092988_Part2/Form1.Designer.cs
+++ MatthewWierenga_ST10092988_Part2/Form1.Designer.cs
@@ -28,128 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblMap = new System.Windows.Forms.Label();
-            this.rtbInfo = new System.Windows.Forms.RichTextBox();
-            this.rtbSelectedEnemy = new System.Windows.Forms.RichTextBox();
-            this.gbxAttack = new System.Windows.Forms.GroupBox();
-            this.cbxEnemies = new System.Windows.Forms.ComboBox();
-            this.btnAttack = new System.Windows.Forms.Button();
-            this.rtbAttackLog = new System.Windows.Forms.RichTextBox();
-            this.btnLeft = new System.Windows.Forms.Button();
-            this.btnUp = new System.Windows.Forms.Button();
             this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
             this.btnDown = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.gbxAttack = new System.Windows.Forms.GroupBox();
+            this.rtbAttackLog = new System.Windows.Forms.RichTextBox();
+            this.btnAttack = new System.Windows.Forms.Button();
+            this.rtbSelectedEnemy = new System.Windows.Forms.RichTextBox();
+            this.cbxEnemies = new System.Windows.Forms.ComboBox();
+            this.rtbInfo = new System.Windows.Forms.RichTextBox();
+            this.lblMap = new System.Windows.Forms.Label();
             this.gbxAttack.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblMap
+            // btnRight
             // 
-            this.lblMap.AutoSize = true;
-            this.lblMap.Location = new System.Drawing.Point(288, 137);
-            this.lblMap.Name = "lblMap";
-            this.lblMap.Size = new System.Drawing.Size(35, 13);
-            this.lblMap.TabIndex = 0;
-            this.lblMap.Text = "label1";
+            this.btnRight.Location = new System.Drawing.Point(887, 527);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(31, 23);
+            this.btnRight.TabIndex = 13;
+            this.btnRight.Text = ">";
+            this.btnRight.UseVisualStyleBackColor = true;
             // 
-            // rtbInfo
+            // btnLeft
             // 
-            this.rtbInfo.Location = new System.Drawing.Point(719, 97);
-            this.rtbInfo.Name = "rtbInfo";
-            this.rtbInfo.Size = new System.Drawing.Size(217, 96);
-            this.rtbInfo.TabIndex = 1;
-            this.rtbInfo.Text = "";
+            this.btnLeft.Location = new System.Drawing.Point(813, 527);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(31, 23);
+            this.btnLeft.TabIndex = 12;
+            this.btnLeft.Text = "<";
+            this.btnLeft.UseVisualStyleBackColor = true;
             // 
-            // rtbSelectedEnemy
+            // btnDown
             // 
-            this.rtbSelectedEnemy.Location = new System.Drawing.Point(0, 55);
-            this.rtbSelectedEnemy.Name = "rtbSelectedEnemy";
-            this.rtbSelectedEnemy.Size = new System.Drawing.Size(217, 96);
-            this.rtbSelectedEnemy.TabIndex = 2;
-            this.rtbSelectedEnemy.Text = "";
+            this.btnDown.Location = new System.Drawing.Point(850, 549);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(31, 23);
+            this.btnDown.TabIndex = 11;
+            this.btnDown.Text = "v";
+            this.btnDown.UseVisualStyleBackColor = true;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(850, 507);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(31, 23);
+            this.btnUp.TabIndex = 10;
+            this.btnUp.Text = "^";
+            this.btnUp.UseVisualStyleBackColor = true;
             // 
             // gbxAttack
             // 
             this.gbxAttack.Controls.Add(this.rtbAttackLog);
             this.gbxAttack.Controls.Add(this.btnAttack);
-            this.gbxAttack.Controls.Add(this.cbxEnemies);
             this.gbxAttack.Controls.Add(this.rtbSelectedEnemy);
-            this.gbxAttack.Location = new System.Drawing.Point(719, 218);
+            this.gbxAttack.Controls.Add(this.cbxEnemies);
+            this.gbxAttack.Location = new System.Drawing.Point(749, 221);
             this.gbxAttack.Name = "gbxAttack";
-            this.gbxAttack.Size = new System.Drawing.Size(217, 342);
-            this.gbxAttack.TabIndex = 3;
+            this.gbxAttack.Size = new System.Drawing.Size(215, 269);
+            this.gbxAttack.TabIndex = 9;
             this.gbxAttack.TabStop = false;
             this.gbxAttack.Text = "Attacking";
+            // 
+            // rtbAttackLog
+            // 
+            this.rtbAttackLog.Location = new System.Drawing.Point(7, 192);
+            this.rtbAttackLog.Name = "rtbAttackLog";
+            this.rtbAttackLog.Size = new System.Drawing.Size(208, 77);
+            this.rtbAttackLog.TabIndex = 3;
+            this.rtbAttackLog.Text = "";
+            // 
+            // btnAttack
+            // 
+            this.btnAttack.Location = new System.Drawing.Point(7, 162);
+            this.btnAttack.Name = "btnAttack";
+            this.btnAttack.Size = new System.Drawing.Size(208, 23);
+            this.btnAttack.TabIndex = 2;
+            this.btnAttack.Text = "Attack";
+            this.btnAttack.UseVisualStyleBackColor = true;
+            // 
+            // rtbSelectedEnemy
+            // 
+            this.rtbSelectedEnemy.Location = new System.Drawing.Point(7, 48);
+            this.rtbSelectedEnemy.Name = "rtbSelectedEnemy";
+            this.rtbSelectedEnemy.Size = new System.Drawing.Size(208, 96);
+            this.rtbSelectedEnemy.TabIndex = 1;
+            this.rtbSelectedEnemy.Text = "";
             // 
             // cbxEnemies
             // 
             this.cbxEnemies.FormattingEnabled = true;
-            this.cbxEnemies.Location = new System.Drawing.Point(7, 28);
+            this.cbxEnemies.Location = new System.Drawing.Point(7, 20);
             this.cbxEnemies.Name = "cbxEnemies";
-            this.cbxEnemies.Size = new System.Drawing.Size(210, 21);
-            this.cbxEnemies.TabIndex = 3;
+            this.cbxEnemies.Size = new System.Drawing.Size(208, 21);
+            this.cbxEnemies.TabIndex = 0;
             // 
-            // btnAttack
+            // rtbInfo
             // 
-            this.btnAttack.Location = new System.Drawing.Point(0, 179);
-            this.btnAttack.Name = "btnAttack";
-            this.btnAttack.Size = new System.Drawing.Size(217, 23);
-            this.btnAttack.TabIndex = 4;
-            this.btnAttack.Text = "Attack";
-            this.btnAttack.UseVisualStyleBackColor = true;
+            this.rtbInfo.Location = new System.Drawing.Point(749, 88);
+            this.rtbInfo.Name = "rtbInfo";
+            this.rtbInfo.Size = new System.Drawing.Size(215, 96);
+            this.rtbInfo.TabIndex = 8;
+            this.rtbInfo.Text = "";
             // 
-            // rtbAttackLog
+            // lblMap
             // 
-            this.rtbAttackLog.Location = new System.Drawing.Point(0, 218);
-            this.rtbAttackLog.Name = "rtbAttackLog";
-            this.rtbAttackLog.Size = new System.Drawing.Size(217, 88);
-            this.rtbAttackLog.TabIndex = 5;
-            this.rtbAttackLog.Text = "";
-            // 
-            // btnLeft
-            // 
-            this.btnLeft.Location = new System.Drawing.Point(769, 606);
-            this.btnLeft.Name = "btnLeft";
-            this.btnLeft.Size = new System.Drawing.Size(31, 23);
-            this.btnLeft.TabIndex = 6;
-            this.btnLeft.Text = "<";
-            this.btnLeft.UseVisualStyleBackColor = true;
-            // 
-            // btnUp
-            // 
-            this.btnUp.Location = new System.Drawing.Point(793, 577);
-            this.btnUp.Name = "btnUp";
-            this.btnUp.Size = new System.Drawing.Size(31, 23);
-            this.btnUp.TabIndex = 7;
-            this.btnUp.Text = "^";
-            this.btnUp.UseVisualStyleBackColor = true;
-            // 
-            // btnRight
-            // 
-            this.btnRight.Location = new System.Drawing.Point(820, 606);
-            this.btnRight.Name = "btnRight";
-            this.btnRight.Size = new System.Drawing.Size(31, 23);
-            this.btnRight.TabIndex = 8;
-            this.btnRight.Text = ">";
-            this.btnRight.UseVisualStyleBackColor = true;
-            // 
-            // btnDown
-            // 
-            this.btnDown.Location = new System.Drawing.Point(793, 635);
-            this.btnDown.Name = "btnDown";
-            this.btnDown.Size = new System.Drawing.Size(31, 23);
-            this.btnDown.TabIndex = 9;
-            this.btnDown.Text = "v";
-            this.btnDown.UseVisualStyleBackColor = true;
+            this.lblMap.AutoSize = true;
+            this.lblMap.Location = new System.Drawing.Point(244, 94);
+            this.lblMap.Name = "lblMap";
+            this.lblMap.Size = new System.Drawing.Size(35, 13);
+            this.lblMap.TabIndex = 7;
+            this.lblMap.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1209, 661);
-            this.Controls.Add(this.btnDown);
             this.Controls.Add(this.btnRight);
-            this.Controls.Add(this.btnUp);
             this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
             this.Controls.Add(this.gbxAttack);
             this.Controls.Add(this.rtbInfo);
             this.Controls.Add(this.lblMap);
@@ -163,17 +163,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblMap;
-        private System.Windows.Forms.RichTextBox rtbInfo;
-        private System.Windows.Forms.RichTextBox rtbSelectedEnemy;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnUp;
         private System.Windows.Forms.GroupBox gbxAttack;
         private System.Windows.Forms.RichTextBox rtbAttackLog;
         private System.Windows.Forms.Button btnAttack;
+        private System.Windows.Forms.RichTextBox rtbSelectedEnemy;
         private System.Windows.Forms.ComboBox cbxEnemies;
-        private System.Windows.Forms.Button btnLeft;
-        private System.Windows.Forms.Button btnUp;
-        private System.Windows.Forms.Button btnRight;
-        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.RichTextBox rtbInfo;
+        private System.Windows.Forms.Label lblMap;
     }
 }
 
--- MatthewWierenga_ST10092988_Part2/Form1.cs
+++ MatthewWierenga_ST10092988_Part2/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MatthewWierenga_ST10092988_Part1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,91 @@ namespace MatthewWierenga_ST10092988_Part2
 {
     public partial class Form1 : Form
     {
+        protected GameEngine GM;
         public Form1()
         {
             InitializeComponent();
+
+            GM = new GameEngine();
+
+            RedrawMap();
+            rtbInfo.Text = GM.GAMEMAP.PLAYERCHARACTER.ToString();
+
+            foreach (Enemy G in GM.GAMEMAP.ENEMIES)
+            {
+                cbxEnemies.Items.Add(G.ToString());
+            }
+
+            cbxEnemies.SelectedIndex = 0;
+            rtbSelectedEnemy.Text = GM.GAMEMAP.ENEMIES[cbxEnemies.SelectedIndex].ToString();
         }
+
+        void RedrawMap()
+        {
+            lblMap.Text = GM.GAMEMAP.ToString();
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            if (GM.MovePlayer(MovementDirection.Right))
+            {
+
+                RedrawMap();
+                GM.GAMEMAP.UpdateVision();
+                rtbInfo.Text = GM.GAMEMAP.PLAYERCHARACTER.ToString();
+            }
+
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            if (GM.MovePlayer(MovementDirection.Up))
+            {
+
+                RedrawMap();
+                GM.GAMEMAP.UpdateVision();
+                rtbInfo.Text = GM.GAMEMAP.PLAYERCHARACTER.ToString();
+            }
+
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            if (GM.MovePlayer(MovementDirection.Left))
+            {
+
+                RedrawMap();
+                GM.GAMEMAP.UpdateVision();
+                rtbInfo.Text = GM.GAMEMAP.PLAYERCHARACTER.ToString();
+            }
+
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+
+            if (GM.MovePlayer(MovementDirection.Down))
+            {
+
+                RedrawMap();
+                GM.GAMEMAP.UpdateVision();
+                rtbInfo.Text = GM.GAMEMAP.PLAYERCHARACTER.ToString();
+            }
+
+        }
+
+        private void cbxEnemies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rtbSelectedEnemy.Text = GM.GAMEMAP.ENEMIES[cbxEnemies.SelectedIndex].ToString();
+        }
+
+        private void btnAttack_Click(object sender, EventArgs e)
+        {
+
+            rtbAttackLog.Text += GM.PlayerAttack(cbxEnemies.SelectedIndex) + "\n \n";
+            rtbSelectedEnemy.Text = GM.GAMEMAP.ENEMIES[cbxEnemies.SelectedIndex].ToString();
+        }
+
     }
 }
+
