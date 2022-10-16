09d46b92ca13a780e0b90658ca647faaa5e4d72f
Matthew Wierenga <matt.wierenga@outlook.com>
2022-10-16 12:41:32 +0200

    Update Tile.cs

--- MatthewWierenga_ST10092988_Part2/Tile.cs
+++ MatthewWierenga_ST10092988_Part2/Tile.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using MatthewWierenga_ST10092988_Part1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MatthewWierenga_ST10092988_Part1
+namespace MatthewWierenga_ST10092988_Part2
 {
     public abstract class Tile
     {
